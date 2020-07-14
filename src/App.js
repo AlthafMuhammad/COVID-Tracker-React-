4599a8d2b9e4dedcbdf0bf52ee1cb9a8ebd8fd49
*Althaf <althafmuhammad@computer.org>
2020-07-14 21:35:46 +0530

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
+import India from './Components/India';
+import World from './Components/World';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom';
+
 
 
 function App() {
   return (
-    <div>
-      <h2>Covid - 19 Status Tracker</h2>
+    <div className="container-fluid">
+      <Router>
+        <Header/>
+        <Switch>
+          <Route exact path="/">
+            <India/>
+          </Route>
+          <Route exact path="/india">
+            <India/>
+          </Route>
+          <Route exact path="/world">
+            <World/>
+          </Route>
+        </Switch>
+        
+      </Router>
       
 
     </div>
---
+++ src/Components/India.js
@@ -0,0 +1,25 @@
+import React,{Component} from 'react';
+
+class India extends Component{
+    render(){
+        return(
+            <div className="row">
+                <div className="col-md-12">
+                    <img src="https://www.countryflags.io/IN/shiny/64.png"/>
+                    <h3>India</h3>
+                </div>
+                <div className="col-md-12">
+                    <div className="row">
+                        <div className="col-md-3">
+                            
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default India;
