83184115a4744679836ab5ea6a20f01ba953e66e
*Althaf <althafmuhammad@computer.org>
2020-07-15 13:38:37 +0530

    Updates

---
+++ precache-manifest.7260b817d4e1fa33f3e563b0a1fbcea7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac547c0ea372485eed1301733df34e38",
+    "url": "/COVID19-Tracker-React/index.html"
+  },
+  {
+    "revision": "db2ce9d9d9023dd44ca3",
+    "url": "/COVID19-Tracker-React/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "158d357021a5dde0aada",
+    "url": "/COVID19-Tracker-React/static/css/main.cb58d7bb.chunk.css"
+  },
+  {
+    "revision": "db2ce9d9d9023dd44ca3",
+    "url": "/COVID19-Tracker-React/static/js/2.9a998c0f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/COVID19-Tracker-React/static/js/2.9a998c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "158d357021a5dde0aada",
+    "url": "/COVID19-Tracker-React/static/js/main.9d645d70.chunk.js"
+  },
+  {
+    "revision": "9537c49a91815c22c7f1",
+    "url": "/COVID19-Tracker-React/static/js/runtime-main.87767ee2.js"
+  }
+]);
