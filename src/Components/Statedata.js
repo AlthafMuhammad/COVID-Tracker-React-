afb58e8fa0c960c5292b80aae862893d60862a09
*Althaf <althafmuhammad@computer.org>
2020-07-15 09:44:45 +0530

    Added State List

--- src/Components/Statedata.js
+++ src/Components/Statedata.js
@@ -6,43 +6,42 @@ class Statedata extends Component{
     constructor(){
         super();
         this.state={
-            stateData:{}
+            stateData: {}
         };
     }
 
   componentDidMount(){
       Axios.get("https://api.covid19india.org/state_district_wise.json")
       .then(response => {
-          let x=response.data
+          var x=response.data
           this.setState({stateData:x});
       });
   }
 
     render(){
+        let keys=Object.keys(this.state.stateData);
         return(
             <div className="row">
                 <div className="col-md-12">
                     <Accordion>
-                        <Card>
-                            <Card.Header>
-                                <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                                    Click me!
-                                </Accordion.Toggle>
-                            </Card.Header>
-                            <Accordion.Collapse eventKey="0">
-                                <Card.Body>Hello! I'm the body</Card.Body>
-                            </Accordion.Collapse>
-                        </Card>
-                        <Card>
-                            <Card.Header>
-                            <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                                Click me!
-                            </Accordion.Toggle>
-                            </Card.Header>
-                            <Accordion.Collapse eventKey="1">
-                            <Card.Body>Hello! I'm another body</Card.Body>
-                            </Accordion.Collapse>
-                        </Card>
+                        {
+                            keys.map((itm,ky)=>{
+                                let districts=this.state.stateData[itm].districtData;
+                                let districtkeys=Object.keys(districts);
+                                return(
+                                    <Card>
+                                        <Card.Header>
+                                            <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                                                {itm}
+                                            </Accordion.Toggle>
+                                        </Card.Header>
+                                        <Accordion.Collapse eventKey="0">
+                                            <Card.Body>Hello! I'm the body</Card.Body>
+                                        </Accordion.Collapse>
+                                    </Card>
+                                )
+                            })
+                        }
                     </Accordion>
                 </div>
             </div>
