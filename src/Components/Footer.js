a5ccbf503ae39c1422e0cfc964ee00313ad89857
*Althaf <althafmuhammad@computer.org>
2020-07-16 11:13:58 +0530

    Added data sources

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,6 +6,11 @@ class Footer extends Component{
         return(
             <div className="col-md-12 bg-dark text-white">
                 <div className="text-center">
+                    <p>
+                        <b>Data Sources</b>: <a href="https://api.covid19india.org/" style={{ textDecoration: 'none', color:'white'}} target="_blank">COVID-19 India Org Data Operations Group</a><br/>
+                        <a href="https://corona.lmao.ninja/" style={{ textDecoration: 'none', color:'white' }} target="_blank">disease.sh - Open Disease Data</a>
+                    </p>
+            
                     Created using React by <a href="https://althafmuhammad.me" style={{ textDecoration: 'none' }}>Althaf Muhammad</a>
                 </div>
             </div>
